4e58c688cb05cf73c872032273fe7b25506e6187
Alistair McKinnell <alistair.mckinnell@gmail.com>
2013-09-09 15:56:41 -0400

    Initial solution commit.

--- FizzBuzzKata/FizzBuzz.cs
+++ FizzBuzzKata/FizzBuzz.cs
@@ -2,5 +2,18 @@
 {
     internal class FizzBuzz
     {
+        public string Classify(int n)
+        {
+            if (n % 3 == 0 && n % 5 == 0)
+                return "fizzbuzz";
+
+            if (n % 5 == 0)
+                return "buzz";
+
+            if (n % 3 == 0)
+                return "fizz";
+
+            return n.ToString();
+        }
     }
 }
--- FizzBuzzKata/FizzBuzzTest.cs
+++ FizzBuzzKata/FizzBuzzTest.cs
@@ -5,10 +5,35 @@ namespace FizzBuzzKata
     [TestClass]
     public class FizzBuzzTest
     {
+        private readonly FizzBuzz sut = new FizzBuzz();
+
         [TestMethod]
-        public void DescribeBehaviour()
+        public void ClassifyRegularNumbersAsIs()
         {
-            Assert.Fail();
+            Assert.AreEqual("1", sut.Classify(1));
+            Assert.AreEqual("2", sut.Classify(2));
+            Assert.AreEqual("4", sut.Classify(4));
+        }
+
+        [TestMethod]
+        public void ClassifyDivisibleBy3AsFizz()
+        {
+            Assert.AreEqual("fizz", sut.Classify(3));
+            Assert.AreEqual("fizz", sut.Classify(6));
+        }
+
+        [TestMethod]
+        public void ClassifyDivisibleBy5AsBuzz()
+        {
+            Assert.AreEqual("buzz", sut.Classify(5));
+            Assert.AreEqual("buzz", sut.Classify(10));
+        }
+
+        [TestMethod]
+        public void ClassifyDivisibleByBoth3And5AsFizzBuzz()
+        {
+            Assert.AreEqual("fizzbuzz", sut.Classify(15));
+            Assert.AreEqual("fizzbuzz", sut.Classify(30));
         }
     }
 }
